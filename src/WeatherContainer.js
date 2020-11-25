920be48fde85a2c812baac4cef81c04247ebc662
Igor Eskin <eskinhd@gmail.com>
2020-11-25 14:44:41 -0500

    Weather icon and favicon

--- src/WeatherContainer.js
+++ src/WeatherContainer.js
@@ -10,7 +10,6 @@ const WeatherContainer = () => {
   }, [])
 
   useEffect(() => {
-    console.log("IN USE EFFECT: ", weather);
     setWeather(weather)
   }, [weather])
 
@@ -18,7 +17,7 @@ const WeatherContainer = () => {
     try {
       const res = await axios
       .get('https://api.openweathermap.org/data/2.5/weather?zip=10036,us&appid=709847967f5e54b97308c1b2cae4dee5');
-      setWeather(res.data)
+      setWeather(res.data);
     } catch (error) {
       console.error(error.message);
     }
@@ -28,13 +27,26 @@ const WeatherContainer = () => {
 
   return (
     <div>
-      <div className="header">{weather.name}</div>
+      <div className="header">{weather.name}
+        {
+          weather.weather && 
+          <img 
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} 
+            alt="weather_icon"
+          />
+        }
+      </div>
+      
       <div className="description">{weather.weather && weather.weather[0].description}</div>
-      <div className="temp">{weather.main && tempConversion(weather.main.temp)}<span className="degree">&deg;</span></div>
-        <div className="minmax">
-          <span>{tempConversion(weather.main && weather.main.temp_min)}<span className="degree">&deg;</span></span>
-          <span>{tempConversion(weather.main && weather.main.temp_max)}<span className="degree">&deg;</span></span>
-        </div>
+
+      <div className="temp">{weather.main && tempConversion(weather.main.temp)}
+        <span className="degree">&deg;</span>
+      </div>
+
+      <div className="minmax">
+        <span>{tempConversion(weather.main && weather.main.temp_min)}<span className="degree">&deg;</span></span>
+        <span>{tempConversion(weather.main && weather.main.temp_max)}<span className="degree">&deg;</span></span>
+      </div>
       
       <div className="line" />
 
