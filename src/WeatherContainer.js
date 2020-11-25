8c47576c2e9c3993f7154ab6eb2683ca2dcc0788
Igor Eskin <eskinhd@gmail.com>
2020-11-25 09:55:16 -0500

    Weather data fetched from the API

--- src/WeatherContainer.js
+++ src/WeatherContainer.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const WeatherContainer = () => {
+
+  const [weather, setWeather] = useState([])
+
+  useEffect(() => {
+    fetchWeather()
+  }, [])
+
+  useEffect(() => {
+    console.log("IN USE EFFECT: ", weather);
+    setWeather(weather)
+  }, [weather])
+
+  const fetchWeather = async () => {
+    try {
+      const res = await axios
+      .get('https://api.openweathermap.org/data/2.5/weather?zip=10036,us&appid=709847967f5e54b97308c1b2cae4dee5');
+      setWeather(res.data)
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  const tempConversion = n => Math.round(n - 273.15);
+
   return (
     <div>
-      <div className="header">New York</div>
-      <div className="description">Scattered Thundershowers</div>
-      <div className="temp">16<span className="degree">&deg;</span></div>
+      <div className="header">{weather.name}</div>
+      <div className="description">{weather.weather && weather.weather[0].description}</div>
+      <div className="temp">{weather.main && tempConversion(weather.main.temp)}<span className="degree">&deg;</span></div>
         <div className="minmax">
-          <span>6<span className="degree">&deg;</span></span>
-          <span>22<span className="degree">&deg;</span></span>
+          <span>{tempConversion(weather.main && weather.main.temp_min)}<span className="degree">&deg;</span></span>
+          <span>{tempConversion(weather.main && weather.main.temp_max)}<span className="degree">&deg;</span></span>
         </div>
       
       <div className="line" />
@@ -17,7 +42,7 @@ const WeatherContainer = () => {
 
       <div className="buttons">
         <div className="zip-container"><span className="zip">10036</span></div>
-        <button className="update">Update</button>
+        <button className="update" onClick={fetchWeather}>Update</button>
       </div>
     </div>
   )
